a875391f2474fea896f9547f6bd2a35653d30f4e
carlos padial <surreal@asturnazari.com>
2017-05-05 00:06:44 +0200

    first working version; stream from instant.io and copy the magnet inside torrent.js

---
+++ torrent/index.md
@@ -0,0 +1,47 @@
+
+<div id='background' class='background'>
+</div>
+
+<div id='main-video-div' class='centered-content'>
+</div>
+
+### Credits
+
+Line and line, a cartesian short story 
+
+by [Julia Olavarrieta](http://estoyhechountrapo.com/) and [Carlos Padial](http://surreal.asturnazari.com)
+
+Translations by **María Velasco**.
+
+Made with [Blender](https://blender.org) and [Ardour](https://ardour.org/)
+
+### Embed it!
+
+Add this code to your html to embed the shotfilm:
+
+    <iframe src="https://surreal6.github.io/linea-y-lineo/projector/" style='width: 100%; height: 900px'></iframe>
+
+### Contribute subtitles
+
+Get [one of this files](https://github.com/surreal6/linea-y-lineo/tree/master/subtitles) and send us a translation via [GitHub](https://github.com/surreal6/linea-y-lineo/issues/new). 
+
+Or if you know how, fork and ask for a pull request <i class="em em-ok_hand"></i>
+
+### License
+
+<div class='license'>
+  <a href="https://creativecommons.org/licenses/by-sa/3.0/es/">
+    <img src="imgs/CC-BY-SA_icon.svg.png" alt='cc-by-sa'>
+    cc-by-sa
+  </a>
+</div>
+
+HOIPOI Cinema Boilerplate - first draft
+
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="https://afeld.github.io/emoji-css/emoji.css">
+
+<!-- Include the latest version of WebTorrent -->
+<script src="https://cdn.jsdelivr.net/webtorrent/latest/webtorrent.min.js"></script>
+
+<script type="text/javascript" src='torrent.js'></script>
---
+++ torrent/torrent.js
@@ -0,0 +1,71 @@
+var client = new WebTorrent()
+
+var torrentId = 'magnet:?xt=urn:btih:f46330192b255f5626494de2290bcc87b2315238&dn=linea-y-lineo.avi&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com'
+
+client.add(torrentId, function (torrent) {
+  // background video
+
+  var file = torrent.files.find(function (file) {
+    return file.name.includes('background')
+  })
+  file.appendTo('#background', function (err, elem) {
+        elem.id = 'background-video';
+        elem.playbackRate = 0.25;
+    })
+
+  // main video
+  var file = torrent.files.find(function (file) {
+    return file.name.endsWith('.webm')
+  })
+  file.appendTo('#main-video-div', function (err, elem) {
+    var backgroundVideo = document.getElementById('background-video');
+    var onPauseFunction = function() {
+        backgroundVideo.style.opacity = 0.3;
+        backgroundVideo.play();
+    };
+    var onPlayFunction = function() {
+        backgroundVideo.style.opacity = 0;
+        backgroundVideo.pause();
+    };
+
+    elem.pause();
+    elem.id = 'main-video';
+    elem.class='video';
+    elem.loop = true;
+    elem.style='width: 100%; outline: 0;';
+    elem.onpause = function() {
+        backgroundVideo.style.opacity = 0.3;
+        backgroundVideo.play();
+    }; 
+    elem.onplay = function() {
+        backgroundVideo.style.opacity = 0;
+        backgroundVideo.pause();
+    };
+
+    track = document.createElement("track"); 
+    track.kind = "captions"; 
+    track.label = "Castellano"; 
+    track.srclang = "es"; 
+    track.mode = "showing"; 
+    track.src = "../subtitles/lineaylineo_es.vtt";
+    elem.appendChild(track);
+    elem.textTracks[0].mode = "showing"; // thanks Firefox    
+
+    track = document.createElement("track"); 
+    track.kind = "captions"; 
+    track.label = "French"; 
+    track.srclang = "fr"; 
+    track.src = "../subtitles/lineaylineo_fr.vtt";
+    elem.appendChild(track);
+
+    var track = document.createElement("track"); 
+    track.kind = "captions"; 
+    track.label = "English"; 
+    track.srclang = "en"; 
+    track.src = "../subtitles/lineaylineo_en.vtt"; 
+    elem.appendChild(track);
+
+
+  })
+})
+
